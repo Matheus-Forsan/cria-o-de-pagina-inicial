2ff68b239d3c164e9f296dd714511171be7f2d0d
matheus marques <matheusforsan@gmail.com>
2024-03-01 15:08:48 -0300

    Componentizando o MyPosts

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import {View} from 'react-native';
 import Infoprofile from './src/components/Infoprofile';
 import img2 from "./src/assets/img.jpg";
+import  img1 from "./src/assets/img1.png";
+import MyPosts from './src/components/MyPosts';
+
 
 export default function App() {
   return (
@@ -11,6 +14,8 @@ export default function App() {
       followers={3}
       following={2}
       />
+
+      <MyPosts postimage={img2} />
     </View>
 
   )
---
+++ src/components/MyPosts/index.js
@@ -0,0 +1,40 @@
+import { MaterialCommunityIcons, MaterialIcons, } from '@expo/vector-icons';
+import { View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+
+
+
+export default function MyPosts(postimage) {
+    return (
+        <View style={styles.container}>
+            <View style={styles.tabs}>
+                <TouchableOpacity>
+                    <MaterialIcons name="grid-on" size={40} color="black" />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <MaterialCommunityIcons name="movie-open-play-outline" size={40} color="black" />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <MaterialCommunityIcons name="account-outline" size={40} color="black" />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.postlist}>
+                <Image source={postimage} style={styles.posts} />
+                <Image source={postimage} style={styles.posts} />
+                <Image source={postimage} style={styles.posts} />
+                <Image source={postimage} style={styles.posts} />
+                <Image source={postimage} style={styles.posts} />
+                <Image source={postimage} style={styles.posts} />
+                <Image source={postimage} style={styles.posts} />
+                <Image source={postimage} style={styles.posts} />
+                <Image source={postimage} style={styles.posts} />
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: { marginTop: 20, width: 400 },
+    tabs: { flexDirection: 'row', justifyContent: 'space-around' },
+    postlist: { flexDirection: "row", justifyContent: 'space-between' ,flexWrap:"wrap"},
+    posts: { marginTop: 20, width: "33.3%", height: 150, backgroundColor: 'black', borderColor: "#ddd", borderWidth: 1 }
+})
